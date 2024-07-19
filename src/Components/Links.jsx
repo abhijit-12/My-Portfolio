674878d5151e440c650bdf4712496cd686ea1a71
Abhijit das <adas0861@gmail.com>
2024-07-19 13:05:44 +0530

    Initial commit

---
+++ src/Components/Links.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Links() {
+  return (
+
+    <>
+      <div className="all-links">
+        <ul>
+          <li>
+           <a href="https://www.linkedin.com/in/abhijit-das-aa4a84190/"> <i class="fa-brands fa-linkedin"></i></a>
+          </li>
+          <li>
+           <a href="https://github.com/abhijit-12"><i class="fa-brands fa-github"></i></a>
+          </li>
+          <li>
+            <a href="/"><i class="fa-brands fa-square-instagram"></i></a>
+          </li>
+          <li>
+          <a href="/"> <i class="fa-brands fa-square-facebook"></i> </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Links;
